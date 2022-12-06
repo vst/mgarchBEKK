be8b5e4321ab339bb95ecff76c73a4ca0a4a6819
Vehbi Sinan Tunalioglu <vst@vsthost.com>
2022-12-06 09:36:54 +0800

    chore(dev): re-configure Nix environment
    - All important Nix expressions are now under `./nix/default.nix`.
    - Switched from telos.nix to vanilla nixpkgs (branch `release-22.11`).

--- nix/default.nix
+++ nix/default.nix
@@ -1,19 +1,57 @@
-{ compiler ? "ghc90"
-, ...
+{ ...
 }:
 
 let
   ## Import sources:
   sources = import ./sources.nix;
 
-  ## Import telosnix:
-  telosnix = import sources.telosnix { };
-
   ## Import nixpkgs:
-  pkgs = import telosnix.pkgs-sources.unstable { };
+  pkgs = import sources.nixpkgs { };
+
+  ## Define R package dependencies:
+  rDeps = with pkgs.rPackages; {
+    ## Dependencies for our package:
+    pkg = [
+      mvtnorm
+      tseries
+    ];
+
+    ## Dependencies for development:
+    dev = [
+      devtools
+      roxygen2
+      spelling
+      testthat
+    ];
+  };
+
+  ## Setup R environment:
+  r = pkgs.rWrapper.override {
+    packages = rDeps.pkg ++ rDeps.dev;
+  };
+
+  ## Setup development shell:
+  shell = pkgs.mkShell
+    {
+      buildInputs = [
+        ## Fancy stuff:
+        pkgs.figlet
+        pkgs.lolcat
+
+        ## TeX stuff:
+        pkgs.texlive.combined.scheme-full
+
+        ## R:
+        r
+      ];
+
+      shellHook = ''
+        figlet -w 999 "mgarchBEKK DEV SHELL" | lolcat -S 42
+      '';
+    };
 in
 {
-  sources = sources;
-  telosnix = telosnix;
   pkgs = pkgs;
+  r = r;
+  shell = shell;
 }
--- shell.nix
+++ shell.nix
@@ -3,34 +3,5 @@
 let
   ## Import this codebase's Nix helper set:
   nix = import ./nix { };
-
-  ## Get packages:
-  pkgs = nix.pkgs;
-
-  ## R environment:
-  r = pkgs.rWrapper.override {
-    packages = with pkgs.rPackages; [
-      devtools
-      mvtnorm
-      roxygen2
-      spelling
-      testthat
-      tseries
-    ];
-  };
 in
-pkgs.mkShell {
-  buildInputs = [
-    ## Fancy stuff:
-    pkgs.figlet
-    pkgs.lolcat
-
-    ## Development:
-    pkgs.texlive.combined.scheme-full
-    r
-  ];
-
-  shellHook = ''
-    figlet -w 999 "mgarchBEKK DEV SHELL" | lolcat -S 42
-  '';
-}
+nix.shell
